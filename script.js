c98dbfe95abe45af6f9417a6b0ff5a86a6d29b39
TsaqifRadhitya <tsaqifradhitya@gmail.com>
2025-03-24 11:59:05 +0700

    Second Change

--- script.js
+++ script.js
@@ -9,10 +9,9 @@ const changeMap = () => {
         (position) => {
             setTimeout(() => {
                 document.querySelector('#Peta h1').innerHTML = 'Lokasi Sekarang'
-            }, 400)
-            console.log(currentLocation)
-            document.querySelector('#Makanan h3').style.display = 'none'
-            document.querySelectorAll('.card').forEach((Element) => Element.style.display = 'block')
+                document.querySelector('#Makanan h3').style.display = 'none'
+                document.querySelectorAll('#Makanan .card').forEach((Element) => Element.style.display = 'flex')
+            }, 500)
             currentLocation = position.coords
             const location = `https://maps.google.com/maps?q=${position.coords.latitude},${position.coords.longitude}&hl=id&z=17&output=embed`
             document.querySelector('#Peta iframe').setAttribute('src', location)
@@ -22,11 +21,18 @@ const changeMap = () => {
 
 const dom = () => {
     document.querySelector('#Home > input').addEventListener('keypress', (e) => {
-        if (e.keyCode === 13) {
+        if (e.keyCode !== 13) return;
+
+        const inputValue = document.querySelector('#Home > input').value        
+
+        if (inputValue !== '' && inputValue.trim() !== '') {
             document.querySelector('#Peta button').click()
             location.href = '#Makanan'
+            return;
         }
-    })
 
+        alert('Harap Mengisi Input Pencarian Dengan Benar')
+    })
     document.querySelector('#Peta button').addEventListener('click', changeMap)
+
 }

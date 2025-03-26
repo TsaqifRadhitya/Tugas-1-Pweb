d57390a9b42d514199e64f19ba981d60c15b4368
TsaqifRadhitya <tsaqifradhitya@gmail.com>
2025-03-26 08:25:09 +0700

    Done

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ window.addEventListener('load', () => {
 const bindurl = () => { }
 
 const changeMap = () => {
+    document.getElementById('Peta').scrollIntoView({ behavior: 'smooth' });
     navigator.geolocation.getCurrentPosition(
-        async (position) => {
-            const response = await fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json&apiKey=e0545b91f3584fb9aebff8dce93cbef4`);
-            const data = await response.json();
-            console.log(data)
-            setTimeout(() => {
+        (position) => {
+            const location = `https://maps.google.com/maps?q=${position.coords.latitude},${position.coords.longitude}&hl=id&z=17&output=embed`
+            document.querySelector('#Peta iframe').setAttribute('src', location)
+            fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json&apiKey=e0545b91f3584fb9aebff8dce93cbef4`).then((response) => response.json()).then((data) => {
                 document.querySelector('#Peta h1').innerHTML = `📍 ${data.results[0].formatted}`
                 document.querySelector('#Makanan h3').style.display = 'none'
                 document.querySelectorAll('#Makanan .card').forEach((Element) => Element.style.display = 'flex')
-            }, 500)
-            currentLocation = position.coords
-            const location = `https://maps.google.com/maps?q=${position.coords.latitude},${position.coords.longitude}&hl=id&z=17&output=embed`
-            document.querySelector('#Peta iframe').setAttribute('src', location)
+                setTimeout(()=> {
+                    document.getElementById('Makanan').scrollIntoView({ behavior: 'smooth' });
+                },1000)
+            });
         }
     )
 }
@@ -43,7 +43,6 @@ const dom = () => {
                     location.href = `https://www.google.com/search?q=${inputValue}`
                 ])
             })
-            document.getElementById('Makanan').scrollIntoView({ behavior: 'smooth' });
             return;
         }
 
